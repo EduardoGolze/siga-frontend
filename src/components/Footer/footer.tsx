809e444362228dbcd0111d0f514e9c8ef107f3d4
Eduardo Golze <57068091+EduardoGolze@users.noreply.github.com>
2025-05-27 17:24:48 -0300

    Delete src directory

--- src/components/Footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>Sistema Integrado de Gestão Acadêmica - SIGA React</p>
-      <p>© {new Date().getFullYear()} - Todos os direitos reservados</p>
-    </footer>
-  );
-}
